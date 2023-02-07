decf2fdb97c0dbd0f1eee849a097b8255bf338d2
Jinash Rouniyar <56365715+Jinash-Rouniyar@users.noreply.github.com>
2023-02-07 19:23:54 +0545

    Add files via upload

---
+++ Evennumbers.java
@@ -0,0 +1,14 @@
+package Tutorials;
+
+public class Evennumbers {
+
+	public static void main(String[] args) {
+		for (int i=3; i<=30;i++) {
+			if (i%2==0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
